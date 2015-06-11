9cc4f2a583ec3cf17b07e62e9a906a2c47acb721
Brit Butler <redline6561@gmail.com>
2015-06-11 11:08:16 -0400

    Add ability to make shortened urls.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,19 @@
 class Link < ActiveRecord::Base
   validates :destination, presence: true
 
+  CHARSET = ('a' .. 'z').to_a + ('A' .. 'Z').to_a + (0..9).to_a
+  KEY_LENGTH = 5
+
+  def self.random_string
+    (1..KEY_LENGTH).map { CHARSET[rand(CHARSET.size)] }.join
+  end
+
+  def self.make_shortr
+    shortr = Link.random_string
+
+    while Link.find_by(shortened: shortr)
+      shortr = Link.random_string
+    end
+    shortr
+  end
 end
